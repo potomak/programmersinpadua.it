c9f1fcf641ea307a1c8ef0a3a9bee3416c55feeb
Alessandro Vendruscolo <alessandro.vendruscolo@gmail.com>
2011-01-20 17:25:53 +0100

    initial commit

---
+++ js/initialize.js
@@ -0,0 +1,20 @@
+function initialize() {
+	var myLatlng = new google.maps.LatLng(45.411617,11.868153);
+	var myOptions = {
+		zoom: 16,
+		center: myLatlng,
+		navigationControl: true,
+		navigationControlOptions: {
+			style: google.maps.NavigationControlStyle.SMALL
+		},
+		mapTypeControl: false,
+		scrollwheel: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(document.getElementById("map"), myOptions);
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title:"Hello World!"
+	});
+}
---
+++ js/pip.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	$.getJSON('./attending_1.json', function(data){
+		for(var i=0; i<data.length; i++) {
+			$('ul.people').append('<li class="person"><img src="https://graph.facebook.com/'+ data[i].id +'/picture" alt="'+ data[i].name +'" title="'+ data[i].name +'"/></li>')
+		}
+	})
+	//console.log(navigator.userAgent);
+	if(navigator.userAgent.indexOf('Safari') > 0 && navigator.userAgent.indexOf('Chrome') == -1) {
+		toggle = function() {
+			$('#logo').toggleClass('animate');
+			$('#logo .front').toggleClass('active');
+			$('#logo .back').toggleClass('active');
+			timeout = setTimeout(toggle, 4000);
+		};
+		timeout = setTimeout(toggle, 4000);
+		$('#logo').hover(function() {
+			clearTimeout(timeout);
+		}, function() {
+			timeout = setTimeout(toggle, 4000);
+		});
+	}
+	else {
+		$('#logo .text').css('-webkit-transform', 'rotateX(0deg)');
+		toggle = function() {
+			if($('#logo .front').is(':hidden')) {
+				$('#logo .front').fadeIn(1000);
+			}
+			else {
+				$('#logo .front').fadeOut(1000);
+			}
+			timeout = setTimeout(toggle, 4000);
+		};
+		timeout = setTimeout(toggle, 4000);
+		$('#logo').hover(function() {
+			clearTimeout(timeout);
+		}, function() {
+			timeout = setTimeout(toggle, 4000);
+		});
+	}
+});
